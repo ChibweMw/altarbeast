b89eee0d4ae0b2d1ce8dcc7441bd7f5e68e411e8
ChibweMw <caleidosymmetry@gmail.com>
2020-09-25 16:47:44 -0700

    First pass Pause menu command selection

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,6 +5,13 @@ export default class Game extends Phaser.Scene
         super('game')
     }
 
+    key_PAUSE
+
+    init ()
+    {
+        this.key_PAUSE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P)
+    }
+
     create()
     {
         console.log('Game Scene Online')
@@ -13,18 +20,20 @@ export default class Game extends Phaser.Scene
         this.scene.launch('ui')
 
         // SCENE CONTROLS - Pause, REstart(D), Menu(D)
-        let key_PAUSE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P)
-        key_PAUSE.setEmitOnRepeat(false)
-
-        key_PAUSE.on('down', () => {
-            console.log('PAUSE BUTTON PRESSED')
-            this.scene.pause()
-            this.scene.run('pause')
-        })
 
         let map = this.make.tilemap({ key: 'level-arena-01' })
         let tiles = map.addTilesetImage('altar-tiles-01', 'tiles-arena')
         let layerStaticPlatform = map.createStaticLayer('platform-solid-static', tiles, 0, 0)
         let layerPlatformDeco = map.createStaticLayer('platform-env-static', tiles, 0, 0)
+    }
+
+    update ()
+    {
+        if (Phaser.Input.Keyboard.JustDown(this.key_PAUSE))
+        {
+            console.log('PAUSE BUTTON PRESSED')
+            this.scene.pause()
+            this.scene.run('pause')
+        }
     }
 }
--- src/scenes/Pause.js
+++ src/scenes/Pause.js
@@ -9,31 +9,49 @@ export default class Pause extends Phaser.Scene
     key_PAUSE_UP
     key_RESTART
     key_MENU
+
     sceneTitleText
+
+    sceneResumeText
+    sceneRestartText
+    sceneReturnToMainMenuText
+
     menuSprite_Cursor
+    UI_cursorTarget
+    menuItems = []
 
     init ()
     {
         this.key_PAUSE_UP = false
     }
-
+    
     create ()
     {
+        // SCENE TITLE
         this.sceneTitleText = this.add.bitmapText(this.game.scale.width / 2, this.game.scale.height * 0.2, 'tentown', 'PAUSED', 24).setOrigin(0.5)
-        this.sceneRestartText = this.add.bitmapText(this.game.scale.width / 2, this.game.scale.height * 0.3, 'tentown', 'Resume (P)', 12).setOrigin(0.5)
+        
+        // SCENE MENU TEXT
+        this.sceneResumeText = this.add.bitmapText(this.game.scale.width / 2, this.game.scale.height * 0.3, 'tentown', 'Resume (P)', 12).setOrigin(0.5)
         this.sceneRestartText = this.add.bitmapText(this.game.scale.width / 2, this.game.scale.height * 0.3 + (14 * 1), 'tentown', 'Retry (R)', 12).setOrigin(0.5)
-        this.sceneRestartText = this.add.bitmapText(this.game.scale.width / 2, this.game.scale.height * 0.3 + (14 * 2), 'tentown', 'Main Menu (M)', 12).setOrigin(0.5)
-        this.menuSprite_Cursor = this.add.sprite(this.game.scale.width * 0.3, this.game.scale.height * 0.3 - 12, 'ui-cursor', 0).setOrigin(0)
-        // SCENE CONTROLS - UnPause
-        this.key_PAUSE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P)
+        this.sceneReturnToMainMenuText = this.add.bitmapText(this.game.scale.width / 2, this.game.scale.height * 0.3 + (14 * 2), 'tentown', 'Main Menu (M)', 12).setOrigin(0.5)
+        
+        this.menuItems = [this.sceneResumeText, this.sceneRestartText, this.sceneReturnToMainMenuText]
+        this.UI_cursorTarget = this.menuItems[0]
+        
+        // SCENE MENU CURSOR IMAGE
+        this.menuSprite_Cursor = this.add.sprite(this.game.scale.width * 0.3, this.UI_cursorTarget.y, 'ui-cursor', 0).setOrigin(1, 0.75)
+        
+        // SCENE CONTROLS - UnPause, Up/Down navigation
+        this.key_CONFIRM = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
+        this.key_uiCursor_UP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
+        this.key_uiCursor_DOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
 
         // DEV COMMANDS        
         this.key_RESTART = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
         this.key_MENU = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M)
 
         this.key_RESTART.once('down', () => {
-            console.log(`RESTART GAME SCENE`)
-            this.scene.start('game')
+            
         })        
 
         this.key_MENU.once('down', () => {
@@ -46,24 +64,56 @@ export default class Pause extends Phaser.Scene
     
     update()
     {
-        if (this.key_PAUSE_UP)
+        if (Phaser.Input.Keyboard.JustDown(this.key_CONFIRM))
         {
-            // console.log('Can UnPAUSE')
-            if (Phaser.Input.Keyboard.JustDown(this.key_PAUSE))
+            switch (this.UI_cursorTarget.text)
             {
-                // console.log('UnPAUSEing')
-                this.scene.stop()
-                this.scene.resume('game')
+                case this.sceneResumeText.text:
+                    console.log(`SELECTED: ${this.sceneResumeText.text}`)
+                    
+                        console.log('UnPAUSEing')
+                        this.scene.stop()
+                        this.scene.resume('game')
+                    
+                    break
+                case this.sceneRestartText.text:
+                    console.log(`SELECTED: ${this.sceneRestartText.text}`)
+                    
+                        console.log(`RESTART GAME SCENE`)
+                        this.scene.start('game')
+                    
+                    break
+                case this.sceneReturnToMainMenuText.text:
+                    console.log(`SELECTED: ${this.sceneReturnToMainMenuText.text}`)
+                    
+                        console.log(`Back to menu button hit`)
+                        this.scene.stop('ui')
+                        this.scene.stop('game')
+                        this.scene.start('menu-main')
+                    
+                    break
+                default:
+                    console.log(`nothing selected, pick something`)
             }
         }
-        else 
+
+        if (Phaser.Input.Keyboard.JustDown(this.key_uiCursor_UP))
         {
-            // console.log('Waiting on Pause button release')
-            if (Phaser.Input.Keyboard.JustUp(this.key_PAUSE))
-            {
-                // console.log('Initial Release to Allow UnPausing')
-                this.key_PAUSE_UP = true
-            }
+            Phaser.Utils.Array.RotateRight(this.menuItems)
+            console.log(`menu items: ${this.menuItems}`)
+            console.log(`cursor up: ${this.UI_cursorTarget.text}`)
         }
+        
+        if (Phaser.Input.Keyboard.JustDown(this.key_uiCursor_DOWN))
+        {
+            Phaser.Utils.Array.RotateLeft(this.menuItems)
+            console.log(`menu items: ${this.menuItems}`)
+            console.log(`cursor down: ${this.UI_cursorTarget.text}`)
+        }
+
+        this.UI_cursorTarget = this.menuItems[0]
+
+        this.menuSprite_Cursor.y = this.UI_cursorTarget.y
+
     }
 }
